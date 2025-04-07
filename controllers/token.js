b4647bdbf6085ac433642eda530465dc361c4a6f
Niloy Islam <Niloy@HubXperts-MacBook-Air.local>
2025-04-07 13:52:06 +0600

    remove secret

--- controllers/token.js
+++ controllers/token.js
@@ -1,10 +1,10 @@
 async function getNextechToken() {
     const bodyData = new URLSearchParams();
     bodyData.append("grant_type", "password");
-    bodyData.append("client_id", "e8f83b98-c06c-4170-b24b-5cc1a407ebd5");
-    bodyData.append("username", "48cf740f-348d-49e6-a60b-dc4fea452160@nextech-api.com");
-    bodyData.append("password", ".kAU_+^'73GMcA9");
-    bodyData.append("resource", "https://select.nextech-api.com/api");
+    bodyData.append("client_id", process.env.NEXTECH_CLIENT_ID);
+    bodyData.append("username", process.env.NEXTECH_USERNAME);
+    bodyData.append("password", process.env.NEXTECH_PASSWORD);
+    bodyData.append("resource", process.env.NEXTECH_RESOURCE);
 
     try {
         const response = await fetch("https://login.microsoftonline.com/a5f8392a-a68f-4d74-a353-aefc87f29848/oauth2/token", {
